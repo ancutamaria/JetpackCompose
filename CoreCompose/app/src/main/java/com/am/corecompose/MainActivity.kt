b4edcc492b120dad81cd14db9309b2c2d110969f
ancuta <ancuta.maria@gmail.com>
2021-05-22 14:19:50 +0300

    CoreCompose -> added rows

--- CoreCompose/app/src/main/java/com/am/corecompose/MainActivity.kt
+++ CoreCompose/app/src/main/java/com/am/corecompose/MainActivity.kt
@@ -3,16 +3,17 @@ package com.am.corecompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
@@ -27,24 +28,46 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MainScreen() {
     Surface(
-        color = Color.Blue,
+        color = Color.DarkGray,
         modifier = Modifier.fillMaxSize()
         ) {
-        Surface(
-            color = Color.Magenta,
-            modifier = Modifier.wrapContentSize()
-        ) {
-            Text(
-                text = "Wrapped content",
-                style = MaterialTheme.typography.h3,
-                color = Color.Yellow,
-            )
+        Row (
+            modifier = Modifier.fillMaxSize(),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            horizontalColoredBar(myColor = Color.Blue)
+            horizontalColoredBar(myColor = Color.Magenta)
+            horizontalColoredBar(myColor = Color.Red)
+            horizontalColoredBar(myColor = Color.Yellow)
+            horizontalColoredBar(myColor = Color.Green)
+            horizontalColoredBar(myColor = Color.Cyan)
         }
     }
+}
+
+@Composable
+fun horizontalColoredBar(myColor: Color) {
+    Surface(
+        color = myColor,
+        modifier = Modifier
+            .height(300.dp)
+            .width(20.dp)
+    ) { }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     MainScreen()
+//    Text(
+//        text = "Wrapped content",
+//        style = MaterialTheme.typography.h5,
+//        color = Color.DarkGray,
+//    )
+//    Text(
+//        text = "2nd wrapped content",
+//        style = MaterialTheme.typography.h5,
+//        color = Color.DarkGray,
+//    )
 }
