244e0bf1c70a808487aa2f36086e290dda3ff5df
ancuta <ancuta.maria@gmail.com>
2021-05-22 12:37:19 +0300

    SimpleTextComposeApp -> Added styles, etc to text + added button

--- SimpleTextComposeApp/app/src/main/java/com/am/simpletextcomposeapp/MainActivity.kt
+++ SimpleTextComposeApp/app/src/main/java/com/am/simpletextcomposeapp/MainActivity.kt
@@ -1,13 +1,27 @@
 package com.am.simpletextcomposeapp
 
+import android.content.Context
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.am.simpletextcomposeapp.ui.theme.SimpleTextComposeAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,22 +31,52 @@ class MainActivity : ComponentActivity() {
             SimpleTextComposeAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+//                    GreetingButton(this)
+                    GreetingText(this, name= "Android")
                 }
             }
         }
     }
 }
 
+
+
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun GreetingText(context: Context,name: String) {
+    Text(
+        text = "Hello $name!",
+        modifier = Modifier
+            .width(250.dp)
+            .height(240.dp)
+            .padding(all = 24.dp)
+            .clickable {
+                Toast
+                    .makeText(context, "trilu", Toast.LENGTH_SHORT)
+                    .show()
+            },
+//        style = TextStyle(
+//            color = Color.Magenta,
+//            fontWeight = FontWeight.Bold,
+//            fontSize = 30.sp
+//        )
+        style = MaterialTheme.typography.h6,
+        fontWeight = FontWeight.Bold,
+        color = Color.Magenta
+    )
+}
+
+@Composable
+fun GreetingButton(context: Context) {
+    Button(onClick = { }) {
+        GreetingText(context = context, name = "Button")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun DefaultPreviewMainActivity() {
     SimpleTextComposeAppTheme {
-        Greeting("Android")
+//        GreetingButton(LocalContext.current)
+        GreetingText(LocalContext.current, "Android")
     }
 }
