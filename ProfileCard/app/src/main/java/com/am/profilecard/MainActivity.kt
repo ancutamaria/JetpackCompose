f29bb1f317cb30449d441329ce3e3d9b9ac79a2a
ancuta <ancuta.maria@gmail.com>
2021-05-23 14:54:05 +0300

    ProfileCard -> Create skeleton for profile card layout; organize imports

--- ProfileCard/app/src/main/java/com/am/profilecard/MainActivity.kt
+++ ProfileCard/app/src/main/java/com/am/profilecard/MainActivity.kt
@@ -8,15 +8,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.am.profilecard.ui.theme.ProfileCardTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
