f48218c8c6f93be61c10efcb4083f6c788ea1dbf
ancuta <ancuta.maria@gmail.com>
2021-05-23 17:55:22 +0300

    ProfileCard -> Use LazyColumn for long lists

--- ProfileCard/app/src/main/java/com/am/profilecard/UserProfile.kt
+++ ProfileCard/app/src/main/java/com/am/profilecard/UserProfile.kt
@@ -3,8 +3,17 @@ package com.am.profilecard
 data class UserProfile constructor(val name: String, val status: Boolean, val drawableId: Int)
 
 val userProfileList = arrayListOf<UserProfile>(
+    UserProfile("Positivity girl", false, R.drawable.profile_picture_1),
+    UserProfile("Harry Potter", false, R.drawable.profile_picture_2),
+    UserProfile("Bumblebee", true, R.drawable.profile_picture_3),
     UserProfile("Positivity girl", true, R.drawable.profile_picture_1),
+    UserProfile("Bumblebee", true, R.drawable.profile_picture_3),
     UserProfile("Harry Potter", false, R.drawable.profile_picture_2),
     UserProfile("Bumblebee", false, R.drawable.profile_picture_3),
+    UserProfile("Positivity girl", false, R.drawable.profile_picture_1),
+    UserProfile("Yogi", true, R.drawable.profile_picture_4),
+    UserProfile("Positivity girl", true, R.drawable.profile_picture_1),
+    UserProfile("Yogi", true, R.drawable.profile_picture_4),
+    UserProfile("Harry Potter", true, R.drawable.profile_picture_2),
     UserProfile("Yogi", true, R.drawable.profile_picture_4),
 )
